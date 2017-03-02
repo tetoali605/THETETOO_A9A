88b48d1be1d9ead2f507a8d5d72b2e6f13d88785
tetoali605 <tetoali605@gmail.com>
2017-03-02 17:22:36 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,21 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "me"
+  },
+  info_text = "💡Welcome To TEAMBOSS  \nFor More Information Subscribe To The Channel @llDEV1ll \n\n https://github.com/moody2020/TEAMBOSS\n\n💡Dev : @TH3BOSS\n\n💡 Dev Bot : @ll60Kllbot\n\n💡 channel : @llDEV1ll\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    226861861
+  }
+}
+return _
+end
